73e33ca61e4b8e38bf6cf42785ddf4728872d684
limnyks <anhelina.oleksiuk.25@kse.org.ua>
2026-06-27 12:16:04 +0300

    додано перевірку кредиту

--- script3.py
+++ script3.py
@@ -1,3 +1,24 @@
-print("Hello World")
-print("Hello World")
-print("Hello World")
+def cul_profit(rev, cost):
+    if cost > rev:
+        return (rev - cost)  - (rev - cost * 0.18)
+rev = 500000
+fi_cost = 3500000
+
+net = cul_profit(rev, fi_cost)
+
+def cred(income, debt):
+    if  income <= 0:
+        return "Refuse: noot enough funds"
+    credit_load = (debt / income ) * 100
+    if credit_load > 40:
+        return False
+    else:
+        return True
+income = 10000
+debt = 500000
+
+
+if cred(income, debt):
+    print("Credit Card is valid")
+else:
+    print("Credit Card is not valid")
